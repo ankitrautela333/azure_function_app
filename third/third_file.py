b9c7fb86023b121879ac74d332e1b38770f2cb33
ankitrautela333 <ankitrautela333@gmail.com>
2024-07-07 17:06:05 +0530

    aisekitaise

--- third/third_file.py
+++ third/third_file.py
@@ -9,3 +9,10 @@ def test_function2(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.New pipeline.')
     value=os.getenv("random_value")
     return func.HttpResponse(f"Hello nhi ho gya push azure function app mein new, {value} :value {datetime.datetime.now()}",status_code=200)
+
+@bp2.function_name(name='aisekitaise')
+@bp2.route(route="aisekitaise",auth_level=func.AuthLevel.ANONYMOUS)
+def test_function2(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.New pipeline.')
+    value=os.getenv("random_value")
+    return func.HttpResponse(f"Hello aisekitaise ho gya push azure function app mein new, {value} :value {datetime.datetime.now()}",status_code=200)

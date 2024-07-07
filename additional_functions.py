d9505bcbf46e3b496af97cc3ef16cc612b675bdf
ankitrautela333 <ankitrautela333@gmail.com>
2024-07-07 15:03:24 +0530

    Changed NEWFILEFUNCTION

--- additional_functions.py
+++ additional_functions.py
@@ -6,4 +6,4 @@ bp=func.Blueprint()
 @bp.route(route="newfileroute",auth_level=func.AuthLevel.ANONYMOUS)
 def test_function(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    return func.HttpResponse(f"Hello, {datetime.datetime.now()}",status_code=200)
+    return func.HttpResponse(f"Hello i am changing it, {datetime.datetime.now()}",status_code=200)

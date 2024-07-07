f97fa3af80d477e16e8e28f05d2f8ebcacef83f4
ankitrautela333 <ankitrautela333@gmail.com>
2024-07-07 16:27:24 +0530

    erfer

--- third/third_file.py
+++ third/third_file.py
@@ -6,6 +6,6 @@ bp2=func.Blueprint()
 @bp2.function_name(name='newfile2')
 @bp2.route(route="newfileroute2",auth_level=func.AuthLevel.ANONYMOUS)
 def test_function2(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.New pipeline')
+    logging.info('Python HTTP trigger function processed a request.New pipeline.')
     value=os.getenv("random_value")
     return func.HttpResponse(f"Hello, {value} :value {datetime.datetime.now()}",status_code=200)
